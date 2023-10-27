a76dc097f46e3d1cf9b232fb6c3f8ff1bb841074
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-26 18:42:40 -0700

    fix server

--- server/models.py
+++ server/models.py
@@ -4,3 +4,19 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Account(db.Model, db.SerializerMixin):
+    __tablename__ = 'accounts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    firstname = db.Column(db.String, nullable=False)
+    middlename = db.Column(db.String)
+    lastname = db.Column(db.String, nullable=False)
+    profile_pic = db.Column(db.String)
+    dob = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    about_me = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Account name: {self.firstname} {self.lastname} DOB: {self.dob} >'
