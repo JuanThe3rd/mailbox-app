60e631d73ed281e67d076c598d04db1ff37dc940
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-28 12:54:28 -0700

    sets up frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Home from './Home';
+import Login from './Login';
+import Messages from './Messages';
+import Contacts from './Contacts';
 
 function App() {
-  return <h1>Phase 4 Project Client</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/messages' component={Messages} />
+        <Route path='/contacts' component={Contacts} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
