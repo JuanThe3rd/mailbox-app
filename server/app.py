4c8f89f9e1170f5981a5fc134afec43882fc0f3b
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-28 12:35:47 -0700

    creates server, needs more data in db

--- server/app.py
+++ server/app.py
@@ -2,21 +2,106 @@
 
 # Standard library imports
 
-# Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
-
-# Local imports
 from config import app, db, api
-# Add your model imports
+import os
 
+from models import Account, Friendship
 
-# Views go here!
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 @app.route('/')
 def index():
-    return '<h1>Phase 4 Project Server</h1>'
+    return '<h1>Mail-Box Server</h1>'
 
+
+class Accounts(Resource):
+    def get(self, id = None):
+        if id:
+            account = Account.query.filter_by(id = id).first()
+
+            if account:
+                return make_response(account.to_dict(), 200)
+            else:
+                return make_response({'error': 'Account not found'}, 404)
+        else:
+            accounts = Account.query.all()
+            return make_response([account.to_dict() for account in accounts], 200)
+
+    def post(self):
+        new_account = Account(**request.json)
+        db.session.add(new_account)
+        db.session.commit()
+        return make_response(new_account.to_dict(), 201)
+
+    def patch(self, id):
+        account = Account.query.filter_by(id = id).first()
+
+        if account:
+            for key, value in request.json.items():
+                setattr(account, key, value)
+                db.session.commit()
+                return make_response(account.to_dict(), 200)
+        else:
+            return make_response({'error': 'Account not found'})
+        
+    def delete(self, id):
+        account = Account.query.filter_by(id = id).first()
+
+        if account:
+            db.session.delete(account)
+            db.session.commit()
+            return make_response({'result': 'Account deleted'}, 200)
+        else:
+            return make_response({'error': 'Account not found'}, 404)
+
+
+class Friendships(Resource):
+    def get(self, id = None):
+        if id:
+            friendship = Friendship.query.filter_by(id = id).first()
+
+            if friendship:
+                return make_response(friendship.to_dict(), 200)
+            else:
+                return make_response({'error': 'Friendship not found'}, 404)
+        else:
+            friendships = Friendship.query.all()
+            return make_response([friendship.to_dict() for friendship in friendships], 200)
+    
+    def post(self):
+        new_friendship = Friendship(**request.json)
+        db.session.add(new_friendship)
+        db.session.commit()
+        return make_response(new_friendship.to_dict(), 201)
+
+    def patch(self, id):
+        friendship = Friendship.query.filter_by(id = id).first()
+
+        if friendship:
+            for key, value in request.json.items():
+                setattr(friendship, key, value)
+                db.session.commit()
+                return make_response(friendship.to_dict(), 200)
+        else:
+            return make_response({'error': 'Friendship not found'})
+        
+    def delete(self, id):
+        friendship = Friendship.query.filter_by(id = id).first()
+
+        if friendship:
+            db.session.delete(friendship)
+            db.session.commit()
+            return make_response({'result': 'Friendship deleted'}, 200)
+        else:
+            return make_response({'error': 'Friendship not found'}, 404)
+
+
+api.add_resource(Accounts, '/accounts', '/accounts/<int:id>')
+api.add_resource(Friendships, '/friendships', '/friendships/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
-class Account(db.Model, db.SerializerMixin):
+class Account(db.Model, SerializerMixin):
     __tablename__ = 'accounts'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -18,5 +19,24 @@ class Account(db.Model, db.SerializerMixin):
     phone_number = db.Column(db.String)
     about_me = db.Column(db.String)
 
+    friendships = db.relationship('Friendship', cascade='all, delete', backref='account')
+
+    serialize_rules = ('-friendships.account',)
+
     def __repr__(self):
-        return f'<Account name: {self.firstname} {self.lastname} DOB: {self.dob} >'
+        return f'<Account Name: {self.firstname} {self.lastname} DOB: {self.dob}/>'
+
+
+class Friendship(db.Model, SerializerMixin):
+    __tablename__ = 'friendships'
+
+    id = db.Column(db.Integer, primary_key=True)
+    chat = db.Column(db.String)
+
+    sender_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+    receiver_id = db.Column(db.Integer)
+
+    serialize_rules = ('-messages.friendship',)
+
+    def __repr__(self):
+        return f'<Friendship first_id: {self.sender_id}, second_id: {self.receiver_id} />'
