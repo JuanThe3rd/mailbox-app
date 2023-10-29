5936d6ffbd5448fd52accbc4bed32add447f0e9e
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-10-29 14:07:12 -0700

    creates login page, needs to fix onSubmit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,24 +7,25 @@ function Home() {
     return (
         <div className='home-page' >
             <div className='log-in-btn-container' >
-                <button className='log-in-btn' onClick={handleClick} >Sign-Up/Log-In</button>
+                <button className='log-in-btn' onClick={login} >Sign-Up/Log-In</button>
             </div>
 
             <div className='logo-container' >
+                <img className='home-logo' src={require('../site-images/logo.png')} alt='Logo' onClick={home} />
+                
                 <div className='home-title-container' >
-                    <h1>Mail-Box</h1>
+                    <h2>Welcome!</h2>
                 </div>
-                <img className='home-logo' alt='logo' src='https://icons.iconarchive.com/icons/martz90/circle/512/messages-icon.png' />
             </div>
 
             <div className='home-info'>
                 <div className='about' >
-                    <h3>About</h3>
+                    <h3>about</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec felis nisi. Pellentesque et urna sed tellus vulputate maximus id semper diam. Curabitur placerat lectus id nunc mattis vulputate. Vivamus ut aliquam risus. Maecenas nec libero viverra, feugiat dui pellentesque, molestie quam. Sed placerat mattis mattis. Nulla facilisi. Nulla bibendum metus ac eros sagittis, et maximus lectus facilisis. Phasellus a velit eu mi consectetur bibendum. Donec varius molestie nisl, eu suscipit lorem volutpat sit amet. Vivamus pretium diam at velit tincidunt, nec rutrum neque fringilla. In aliquet egestas arcu. Nunc urna tellus, vehicula posuere sapien vel, vestibulum tempus purus. Praesent vestibulum pharetra suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
 
                 <div className='socials' >
-                    <h3>Contact Us</h3>
+                    <h3>contact us</h3>
                     <p><img className='icon' alt='phone' src='https://static.vecteezy.com/system/resources/previews/019/923/706/original/telephone-and-mobile-phone-icon-calling-icon-transparent-background-free-png.png' /> (555) 555-5555 </p>
 
                     <p><img className='icon' alt='email' src='https://static-00.iconduck.com/assets.00/email-icon-2048x2048-iz8kl1eg.png' /> loremipsum@gmail.com</p>
@@ -35,9 +36,15 @@ function Home() {
         </div>
     )
 
-    function handleClick(e){
+    function login(e){
         history.push({
             pathname: '/login'
+        })
+    }
+
+    function home(e){
+        history.push({
+            pathanem: '/'
         })
     }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
 
 function Login() {
-    return(<h1>Login Page</h1>)
+    const history = useHistory();
+    const [form, setForm] = useState('register');
+    const [formData, setFormData] = useState({})
+
+    console.log(formData)
+
+    return(
+        <div>
+            <img className='logo' src={require('../site-images/favicon.png')} alt='Logo' onClick={home} />
+            
+            <h1 className='login-title' >Login Page</h1>
+            <div className='login-container'>
+                {form === 'login' && 
+                    <form onSubmit={handleSubmit}>
+                        <label>Username:</label>
+                        <br />
+                        <input placeholder='Username' name='username' onChange={handleChange} value={formData['username']} />
+                        <br />
+                        <label>Password:</label>
+                        <br />
+                        <input placeholder='Password' type='password' name='password' onChange={handleChange} value={formData['password']} />
+                        <br />
+                        <input type='Submit' value='Log-In' />
+                    </form>
+                }
+                {form === 'register' &&
+                    <form onSubmit={handleSubmit}>
+                        
+                    </form>
+                }
+            </div>
+            {form === 'register' &&
+                <div>
+                    <label>Already have an account?</label>
+                    <button onClick={() => setForm('login')} >Log-In</button>
+                </div>
+            }
+            {form === 'login' &&
+                <div>
+                    <label>Need an account?</label>
+                    <button onClick={() => setForm('register')} >Register</button>
+                </div>
+            }
+        </div>
+    )
+
+    function home(){
+        history.push({
+            pathname: '/'
+        })
+    }
+
+    function handleChange(e){
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        if (form === 'login'){
+            /*
+                Make get request to validate login form data is in db.
+            */
+        } else if (form === 'register'){
+            /*
+                Make post request to push new form data into db.
+            */
+        }
+    }
 }
 
 export default Login;
